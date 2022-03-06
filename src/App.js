95bb9b903fa501b192e8465241804eec43c37e3d
yugmani <yugmani@yahoo.com>
2022-03-06 10:22:46 -0800

    first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
+  const Message = (props) => <div style={message}>{props.text}</div>;
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="container">
+      <Message text="Hello World" />     
+      {Message({ text: 'Goodbye World!' })}
     </div>
   );
 }
+
+
+
+const message = {
+  color: 'blue',
+  fontSize: '20px',
+  padding: '10px',
+  backgroundColor: 'whitesmoke',
+};
